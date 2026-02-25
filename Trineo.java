264ad87af58712b7e9f24d47454968a2fab7d065
ManelLlurda <manelllurda@alumnes.ilerna.com>
2026-02-25 18:57:29 +0100

    Add files via upload
    Primeras clases del juego(Alguna requiere modificación)

---
+++ Trineo.java
@@ -0,0 +1,13 @@
+package JuegoPinguino;
+
+public class Trineo extends Casilla{
+
+	public Trineo(int posicion) {
+	setPosicion(posicion);
+}	
+	 @Override
+	    public void realizarAccion(Partida partida, Jugador jugador) {
+	        System.out.println("¡Un trineo aparece!");
+	        jugador.moverPosicion(3);
+	    }
+}
