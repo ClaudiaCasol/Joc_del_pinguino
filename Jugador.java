df7a3b3d7a596afb60468daa5b3efdfb7a0cd233
ManelLlurda <manelllurda@alumnes.ilerna.com>
2026-02-25 19:01:29 +0100

    Add files via upload

--- Jugador.java
+++ Jugador.java
@@ -1,5 +1,41 @@
 package JuegoPinguino;
 
 public abstract class Jugador {
+private int posicion=0;
+private String nombre="";
+private String color="";
 
+public Jugador(int posicion,String nombre,String color) {
+	this.posicion=posicion;
+	this.nombre=nombre;
+	this.color=color;
+}
+
+public int getPosicion() {
+	return posicion;
+}
+
+public void setPosicion(int posicion) {
+	this.posicion = posicion;
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public String getColor() {
+	return color;
+}
+
+public void setColor(String color) {
+	this.color = color;
+}
+ public void moverPosicion(int cantidad) {
+	 this.posicion+=cantidad;
+	 if (this.posicion<0) this.posicion=0;
+ }
 }
