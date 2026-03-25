83d317632b3d39fd2c727e31374ae0d5b63cd7cd
ManelLlurda <manelllurda@alumnes.ilerna.com>
2026-03-25 19:13:12 +0100

    Update module-info.java

--- Joc_del_pingu/src/module-info.java
+++ Joc_del_pingu/src/module-info.java
@@ -2,11 +2,8 @@ module Joc_del_pingu {
     requires javafx.controls;
     requires javafx.fxml;
 	requires javafx.media;
-<<<<<<< Updated upstream
 	requires javafx.graphics;
-=======
-	requires javafx.media;
->>>>>>> Stashed changes
+
 
     opens Vistas to javafx.fxml;
     opens Modelos to javafx.graphics;
